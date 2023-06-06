fe1f319cba378340d4c8ccf56a78610485194b9c
Spencer Wilson <5624115+spencerwilson@users.noreply.github.com>
2023-06-06 15:31:54 -0700

    feat(describe): Obtain metadata from Snowflake (#12)

--- src/command/snowflake.rs
+++ src/command/snowflake.rs
@@ -1,11 +1,60 @@
-pub mod dpm_agent {
+use std::env;
+
+use serde::Deserialize;
+
+mod dpm_agent {
     tonic::include_proto!("dpm_agent");
 }
-use dpm_agent::dpm_agent_client::DpmAgentClient;
+use dpm_agent::{dpm_agent_client::DpmAgentClient, query, Query, SnowflakeConnectionParams};
 
-pub struct SnowflakeDescription {
-    // database: String,
-    // tables: Vec<Table>,
+use crate::command::snowflake::dpm_agent::query::SelectExpression;
+
+pub struct SnowflakeDescription {}
+
+/// AFAICT this is the best that can be done with `#[derive(Deserialize)]`. An
+/// improvement on this would be to have `data_type` deserialized into an enum
+/// `DataType` whose variants store type-specific parameters
+/// (`character_maximum_length` for `Varchar`, etc.). That might require a
+/// handwritten `Deserialize` impl, though.
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+#[allow(dead_code)] // TODO(PAT-3375): Remove
+struct InformationSchemaColumnsRow {
+    /// Database that the table belongs to
+    table_catalog: String,
+    /// Schema that the table belongs to
+    table_schema: String,
+    /// Table or View that the column belongs to
+    table_name: String,
+    /// Name of the column
+    column_name: String,
+    /// Ordinal position of the column in the table
+    ordinal_position: f32,
+    /// "YES" if the column may contain NULL, "NO" otherwise
+    /// TODO: Add #[serde(deserialize_with = "...")] attribute
+    is_nullable: String,
+    /// Data type of the column
+    /// TODO: Add #[serde(deserialize_with = "...")] attribute, OR use enum with unit variants
+    data_type: String,
+    /// Maximum length in characters of string columns.
+    /// In Snowflake's VARCHAR(n Unicode code points), n <= 16_777_216 == 2.pow(24).
+    character_maximum_length: Option<f32>,
+    /// Numeric precision of numeric columns
+    numeric_precision: Option<f32>,
+    /// Radix of precision of numeric columns
+    numeric_precision_radix: Option<f32>,
+    /// Scale of numeric columns
+    numeric_scale: Option<f32>,
+    /// Comment for this column
+    comment: Option<String>,
+}
+
+fn field_ref_expression(field_name: &str) -> query::Expression {
+    query::Expression {
+        ex_type: Some(query::expression::ExType::Field(query::FieldReference {
+            field_name: field_name.into(),
+        })),
+    }
 }
 
 pub async fn describe(
@@ -13,19 +62,93 @@ pub async fn describe(
     _schemas: Vec<String>,
     _output: Option<String>,
 ) -> SnowflakeDescription {
-    // Read connection params from env vars.
-    // Instantiate client.
-    // Run introspection query, obtain SnowflakeDescription
+    let grpc_url = format!(
+        "http://{}:{}",
+        env::var("DPM_AGENT_HOST").unwrap_or("[::1]".into()),
+        env::var("DPM_AGENT_PORT").unwrap_or("50051".into())
+    );
 
-    let grpc_host = std::env::var("DPM_AGENT_HOST").unwrap_or("[::1]".to_string());
-    let grpc_port = std::env::var("DPM_AGENT_PORT").unwrap_or("50051".to_string());
-    let grpc_url = format!("http://{}:{}", grpc_host, grpc_port);
+    println!("connecting to dpm-agent at {} ...", grpc_url);
+    let mut client = match DpmAgentClient::connect(grpc_url).await {
+        Ok(client) => client,
+        Err(e) => panic!("connection failed: {:?}", e),
+    };
+    println!("connected to dpm-agent");
 
-    println!("connecting to {} ...", grpc_url);
-    match DpmAgentClient::connect(grpc_url).await {
-        Ok(_) => println!("connected!"),
-        Err(e) => println!("connection failed: {:?}", e),
-    }
+    let connection_params =
+        dpm_agent::connection_request::ConnectionParams::SnowflakeConnectionParams(
+            SnowflakeConnectionParams {
+                account: env::var("SNOWSQL_ACCOUNT").unwrap(),
+                database: env::var("SNOWSQL_DATABASE").unwrap(),
+                user: env::var("SNOWSQL_USER").unwrap(),
+                password: env::var("SNOWSQL_PWD").unwrap(),
+                schema: "INFORMATION_SCHEMA".into(),
+            },
+        );
+    let req = tonic::Request::new(dpm_agent::ConnectionRequest {
+        connection_params: Some(connection_params),
+    });
+
+    println!("creating connection");
+    let connection_response = client.create_connection(req).await.unwrap().into_inner();
+    println!("connection created");
+
+    let select: Vec<SelectExpression> = [
+        "table_catalog",
+        "table_schema",
+        "table_name",
+        "column_name",
+        "ordinal_position",
+        "is_nullable",
+        "data_type",
+        "character_maximum_length",
+        "numeric_precision",
+        "numeric_precision_radix",
+        "numeric_scale",
+        "comment",
+    ]
+    .iter()
+    .map(|c| c.to_uppercase())
+    .map(|c| query::SelectExpression {
+        argument: Some(field_ref_expression(&c)),
+        alias: None,
+    })
+    .collect();
+
+    let order_by: Vec<query::OrderByExpression> = [
+        "table_catalog",
+        "table_schema",
+        "table_name",
+        "ordinal_position",
+    ]
+    .iter()
+    .map(|c| c.to_uppercase())
+    .map(|c| query::OrderByExpression {
+        argument: Some(field_ref_expression(&c)),
+        direction: None,
+    })
+    .collect();
+
+    let introspection = Query {
+        connection_id: connection_response.connection_id,
+        select_from: "COLUMNS".into(),
+        select,
+        filter: None,
+        group_by: vec![],
+        order_by,
+        limit: None,
+    };
+
+    println!("introspecting ...");
+    let intr = client
+        .execute_query(introspection)
+        .await
+        .unwrap()
+        .into_inner();
+
+    let data: Vec<InformationSchemaColumnsRow> =
+        serde_json::from_str(intr.json_data.as_str()).unwrap();
+    println!("{:?}", data);
 
     SnowflakeDescription {}
 }
