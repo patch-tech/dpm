bb9e38db5f03de077647ba5e601b301b6102d703
Gunman108 <41013296+Gunman108@users.noreply.github.com>
2023-06-12 16:43:20 -0700

    python bugfix and dataset generation

--- src/codegen/python.rs
+++ src/codegen/python.rs
@@ -5,6 +5,7 @@ use std::collections::{HashMap, HashSet};
 use super::generator::{DynamicAsset, Generator, ItemRef, Manifest, StaticAsset};
 use crate::descriptor::{DataPackage, DataResource, TableSchema, TableSchemaField};
 use convert_case::{Case, Casing};
+use regress::Regex;
 use rust_embed::RustEmbed;
 use serde::Serialize;
 use std::path::Path;
@@ -238,6 +239,24 @@ impl<'a> Python<'a> {
 impl Generator for Python<'_> {
     fn data_package(&self) -> &DataPackage {
         return &self.data_package;
+    }
+
+    fn dataset_definition(&self) -> DynamicAsset {
+        let dp = self.data_package();
+        let name = dp.name.as_ref().unwrap().to_string();
+        let package_name = self.package_name(&name);
+        let dataset_ref = self.variable_name(&package_name.as_str());
+        let dataset_path = self.file_name(&package_name.as_str());
+        let version = dp.version.to_string();
+        DynamicAsset {
+            path: dataset_path,
+            name: name.to_string(),
+            content: format!(
+                "from dataset import Dataset\n
+                {dataset_ref} = Dataset({:?}, \"{:?}\")",
+                name, version
+            ),
+        }
     }
 
     fn resource_table(&self, r: &DataResource) -> DynamicAsset {
