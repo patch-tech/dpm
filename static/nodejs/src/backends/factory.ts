8bad6f1512cc04e6ba542abb857e0f8e5b80c0f8
Ajith Mascarenhas <ajith@patch.tech>
2023-06-27 16:35:32 -0700

    doc(pat-3368): docstrings for backends files

--- static/nodejs/src/backends/factory.ts
+++ static/nodejs/src/backends/factory.ts
@@ -1,4 +1,6 @@
-// Factory to create the execution backend instance based on the query table's source.
+/**
+ * Factory to create the execution backend instance based on the query table's source.
+ */
 import { Table } from '../table';
 import { getEnv } from './env';
 import { Backend } from './interface';
@@ -11,11 +13,16 @@ enum SourceType {
   SNOWFLAKE,
 }
 
+/**
+ * Returns the source type of the input source url
+ * @param source A valid URL string pointing to the source of the data resource.
+ * @returns A source type, if supported; unknown if not supported.
+ */
 function getSourceType(source: string): SourceType {
   let url: URL | undefined;
   try {
     url = new URL(source);
-  } catch (e) { }
+  } catch (e) {}
 
   if (url?.hostname === 'api.patch.tech' && url.pathname === '/query/graphql') {
     return SourceType.PATCH_GRAPHQL;
@@ -26,12 +33,14 @@ function getSourceType(source: string): SourceType {
   return SourceType.UNKNOWN;
 }
 
-export function makeBackend(query: Table): Backend | undefined {
-  const {
-    datasetVersion: version,
-    name,
-    source,
-  } = query;
+/**
+ * Makes an instance of the backend that can communicate with the source that
+ * holds the table's data.
+ * @param table Table expression that can be executed against the created backend.
+ * @returns A Backend instance or undefined if the source is not supported.
+ */
+export function makeBackend(table: Table): Backend | undefined {
+  const { datasetVersion: version, name, source } = table;
   if (!source) {
     throw new Error(
       'Cannot get execution backend for query with unknown source'
