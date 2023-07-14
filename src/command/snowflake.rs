ae172bd5a1e243615ed5ca5ad56aea8f05ab54f9
Spencer Wilson <5624115+spencerwilson@users.noreply.github.com>
2023-07-13 20:07:04 -0700

    feat(describe): Support DPM_AGENT_URL env var, which may be either secure or not

--- src/command/snowflake.rs
+++ src/command/snowflake.rs
@@ -4,6 +4,7 @@ use chrono::Utc;
 use regress::Regex;
 use serde::Deserialize;
 use tonic::transport::{Channel, ClientTlsConfig};
+use url::Url;
 
 use crate::command::snowflake::dpm_agent::query::SelectExpression;
 use crate::descriptor::{
@@ -104,15 +105,18 @@ pub async fn describe(
     tables: Vec<String>,
     schemas: Vec<String>,
 ) -> DataPackage {
-    let tls = ClientTlsConfig::new().domain_name("agent.dpm.sh");
-    let channel = match Channel::from_static("https://agent.dpm.sh")
-        .tls_config(tls)
-        .unwrap()
-        .connect()
-        .await
-    {
+    let agent_url = env::var("DPM_AGENT_URL").unwrap_or("https://agent.dpm.sh".into());
+    let agent_url = Url::parse(&agent_url)
+        .unwrap_or_else(|_| panic!("DPM_AGENT_URL value not a valid URL: {}", agent_url));
+
+    let mut endpoint = Channel::from_shared(agent_url.as_str().to_string()).unwrap();
+    if agent_url.scheme() == "https" {
+        let tls = ClientTlsConfig::new().domain_name(agent_url.host_str().unwrap());
+        endpoint = endpoint.tls_config(tls).unwrap();
+    }
+    let channel = match endpoint.connect().await {
         Ok(channel) => {
-            eprintln!("connected to https://agent.dpm.sh");
+            eprintln!("connected to {}", agent_url);
             channel
         }
         Err(e) => panic!("connection failed: {:?}", e),
