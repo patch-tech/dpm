f5ecc2c8ba88f3db8973fd92c970eb084fdacdd8
Gunman108 <41013296+gunnerpeterson@users.noreply.github.com>
2023-07-17 14:09:22 -0400

    improve(pat-3670): automate token refresh on `dpm describe patch`

--- src/command/patch.rs
+++ src/command/patch.rs
@@ -1,7 +1,11 @@
 use directories::ProjectDirs;
 use serde_json::{self, Value};
 use std::collections::{BTreeMap, HashMap};
+use std::env;
 use std::fs::read_to_string;
+use std::io::Read;
+use std::path::Path;
+use std::process::{Command, Stdio};
 
 use chrono::Utc;
 use serde::Deserialize;
@@ -120,8 +124,43 @@ pub async fn describe(package_name: String, dataset: String) -> DataPackage {
     package
 }
 
+pub fn exec_cmd(path: &Path, cmd: &str, args: &[&str]) -> String {
+    let mut cmd_binding = Command::new(cmd);
+    let cmd = cmd_binding.current_dir(path).args(args);
+
+    let cmd_output = cmd
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute command");
+
+    let mut stdout = cmd_output.stdout.expect("Failed to capture command output");
+    let mut output = String::new();
+    stdout
+        .read_to_string(&mut output)
+        .expect("Failed to read command output");
+
+    assert!(
+        cmd.output()
+            .expect("Failed to execute command")
+            .status
+            .success(),
+        "Command failed with output:\n{}",
+        output
+    );
+    output
+}
+
+fn refresh_token() {
+    if let Ok(curr_dir) = env::current_dir() {
+        exec_cmd(&curr_dir, "pat", &["access", "token"]);
+    } else {
+        eprintln!("Failed to get current directory");
+    }
+}
+
 /// Queries the Patch config api for a dataset by name, returning dataset schema information
 async fn introspection_query(patch_credentials: PatchCredentials, dataset: &str) -> PatchResponse {
+    refresh_token();
     let query = serde_json::json!({
         "query": format!("
         {{
@@ -155,12 +194,6 @@ async fn introspection_query(patch_credentials: PatchCredentials, dataset: &str)
         .text()
         .await
         .expect("could not get body");
-    // TODO(PAT-3670): refresh the token automatically by running `pat access token` from dpm
-    if body.contains("Token is expired") {
-        panic!(
-            "pat access token has expired. Obtain a fresh token by running `pat access token`, then rerun `dpm describe`"
-        );
-    }
     let response: PatchResponse = serde_json::from_str(&body).unwrap_or_else(|_| {
         panic!(
             "did not receive a valid introspection response; got: {}",
