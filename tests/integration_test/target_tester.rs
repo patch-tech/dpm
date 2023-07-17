2b081a40f832d4bed4c8c17ac047614aa1e49dde
Gunman108 <41013296+gunnerpeterson@users.noreply.github.com>
2023-07-17 13:41:35 -0400

    test(pat-5361): add `dpm describe snowflake` to integration test suite

--- tests/integration_test/target_tester.rs
+++ tests/integration_test/target_tester.rs
@@ -1,3 +1,6 @@
+use serde_json::Value;
+use std::env;
+use std::fs::{read_to_string, File};
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
@@ -28,6 +31,66 @@ pub fn exec_cmd(path: &Path, cmd: &str, args: &[&str]) -> String {
     output
 }
 
+pub fn describe_snowflake(current_dir: &PathBuf) {
+    let generated_dir = current_dir.join(Path::new("./tests/resources/generated"));
+    if env::var("SNOWSQL_ACCOUNT").is_ok()
+        && env::var("SNOWSQL_USER").is_ok()
+        && env::var("SNOWSQL_PWD").is_ok()
+        && env::var("SNOWSQL_DATABASE").is_ok()
+        && env::var("SNOWSQL_SCHEMA").is_ok()
+    {
+        exec_cmd(
+            &generated_dir,
+            "cargo",
+            &[
+                "run",
+                "describe",
+                "snowflake",
+                "--name",
+                "test-snowflake",
+                "--schema",
+                "PUBLIC",
+            ],
+        );
+    } else {
+        exec_cmd(
+            &generated_dir,
+            "bash",
+            &[
+                "-e",
+                "-c",
+                "sops exec-env ../../../secrets/dpm.enc.env 'cargo run describe snowflake --name test-snowflake --schema PUBLIC'",
+            ],
+        );
+    }
+
+    // assert generated directory is not empty
+    assert!(std::path::Path::new("./tests/resources/generated/datapackage.json").exists());
+    let datapackage_contents =
+        read_to_string(Path::new("./tests/resources/generated/datapackage.json"))
+            .expect("Failed to read descriptor contents");
+    let data_package: Value =
+        serde_json::from_str(&datapackage_contents).expect("Unable to parse JSON");
+
+    // assert values in datapackage are correct (name, version, profile of first table)
+    match &data_package {
+        Value::Object(map) => {
+            let name = map.get("name");
+            let version = map.get("version");
+            let first_table_name = map
+                .get("resources")
+                .expect("Key 'resources' does not exist")[0]
+                .get("profile");
+            assert_eq!(name.expect("Key 'name' does not exist"), "test-snowflake");
+            assert_eq!(version.expect("Key 'version' does not exist"), "0.1.0");
+            assert_eq!(
+                first_table_name.expect("Key 'profile' does not exist in first resource"),
+                "data-resource"
+            )
+        }
+        _ => panic!("Not an object"),
+    }
+}
 pub trait TargetTester {
     /// Builds a data package in target language and checks for its existance
     fn build_patch(&self, dir: &PathBuf);
