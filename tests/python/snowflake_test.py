9d1841b450794684f6359951dfd37fd6ab5a4585
Gunman108 <41013296+gunnerpeterson@users.noreply.github.com>
2023-07-18 16:45:05 -0400

    testing compiled results in python snowflake test

--- tests/python/snowflake_test.py
+++ tests/python/snowflake_test.py
@@ -12,6 +12,15 @@ async def test_results():
         UkRealEstateRecords.fields.date_of_transfer,
     ]
 
+    compiled_results = (
+        await UkRealEstateRecords.select(county, city, date_of_transfer)
+        .filter(
+            (county == "CAMBRIDGESHIRE") & (city == "HUNTINGDON") & date_of_transfer.before(date(2017, 1, 1))
+        )
+        .order_by([date_of_transfer, "DESC"])
+        .limit(3)
+        .compile()
+    )
     executed_results = (
         await UkRealEstateRecords.select(county, city, date_of_transfer)
         .filter(
