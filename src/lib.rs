46f18db9f87ffef759c3570707924d8dffc48ef4
Ajith Mascarenhas <ajith@patch.tech>
2023-07-19 19:42:45 -0700

    feat(pat-3672): send dpm version, git sha in user-agent (#80)
    - Include custom user-agent with `dpm` calls to `dpm-agent`: `dpm/<package-version> (<git-short-sha> {dirty?})`
    
    ### Test plan
    1. Modified local `dpm-agent` to log user-agent: `logger.info(context.invocation_metadata())`
    2. Ran a describe command pointed to local `dpm-agent`.
    2. Verified log message was as expected:
    ```bash
    ...
    INFO:__main__:(_Metadatum(key='user-agent', value='dpm/0.1.0 (f46e773-dirty) tonic/0.9.2'),)
    ...
    ```

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,8 @@ mod descriptor;
 
 #[doc(hidden)]
 pub use command::App;
+
+pub mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
