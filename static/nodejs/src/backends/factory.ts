fc66141360aa7067b8f4ba7d73668b0ef385c72d
Gunner Peterson <41013296+gunnerpeterson@users.noreply.github.com>
2023-08-08 13:18:20 -0400

    feat(pat-3824): discover dpm cloud token from session.json - Typescript (#102)
    Tested by console logging results from `getDpmAuthToken` with sample
    `tech.patch.dpm/session.json` directory and no environment variable.

--- static/nodejs/src/backends/factory.ts
+++ static/nodejs/src/backends/factory.ts
@@ -6,6 +6,9 @@ import { getEnv } from './env';
 import { Backend } from './interface';
 import { Patch } from './patch';
 import { Snowflake } from './snowflake';
+import * as fs from 'fs';
+import os from 'os';
+import path from 'node:path';
 
 enum SourceType {
   UNKNOWN,
@@ -36,16 +39,40 @@ function getSourceType(source: string): SourceType {
 /**
  * Discovers the `dpm` authentication token by inspecting:
  * 1. Environment variable DPM_AUTH_TOKEN
- * 2. The session.json file stored by `dpm login`.
- * 3. ...
- */
-function getDpmAuthToken(): string | undefined {
-  let dpmAuthToken = getEnv('DPM_AUTH_TOKEN', undefined);
-  if (dpmAuthToken !== undefined) {
-    return dpmAuthToken;
-  }
+  * 2. The session.json file stored by `dpm login`.
+  * 3. ...
+  */
+export function getDpmAuthToken(): string | undefined {
+  try {
+    let dpmAuthToken = getEnv('DPM_AUTH_TOKEN', undefined);
+    if (dpmAuthToken !== undefined) {
+      return dpmAuthToken;
+    }
+  } catch {
+    interface Session {
+      access_token: string;
+      token_type: number;
+      expires_in: string;
+      scope: string;
+    }
+    let rootDir = os.homedir()
+    let sessionPath = ''
 
-  // TODO(PAT-3824): Look in session.json
+    if (process.platform == 'darwin') {
+      sessionPath = path.join(rootDir, 'Library', 'Application Support', 'tech.patch.dpm', 'session.json')
+    } else if (process.platform == 'win32') {
+      sessionPath = path.join(rootDir, 'AppData', 'Roaming', 'patch', 'session.json')
+    } else if (process.platform == 'linux') {
+      sessionPath = path.join(rootDir, '.config', 'dpm', 'session.json')
+    }
+    try {
+      const sessionString = fs.readFileSync(sessionPath, 'utf-8');
+      const sessionData: Session = JSON.parse(sessionString);
+      return sessionData.access_token
+    } catch (err) {
+      console.error("error recieving access token from project directory:", err);
+    }
+  }
 }
 
 /**
