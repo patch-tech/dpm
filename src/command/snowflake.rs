4d5a5f3a447a14db71aaa492016c1b02425f5013
Ajith Mascarenhas <ajith@patch.tech>
2023-08-11 10:33:27 -0700

    fix(dpm): bad regexp

--- src/command/snowflake.rs
+++ src/command/snowflake.rs
@@ -57,12 +57,12 @@ impl std::str::FromStr for OrganizationName {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         // Source: https://docs.snowflake.com/en/user-guide/admin-account-identifier#organization-name
         // We assume that when ^ refers to "letters" that means [a-zA-Z].
-        if regress::Regex::new("^[a-zA-Z][a-zA-Z0-9]*)$")
+        if regress::Regex::new("^[a-zA-Z][a-zA-Z0-9]*$")
             .unwrap()
             .find(s)
             .is_none()
         {
-            bail!("doesn't match pattern \"^[a-zA-Z][a-zA-Z0-9]*)$\"");
+            bail!("doesn't match pattern \"^[a-zA-Z][a-zA-Z0-9]*$\"");
         }
         Ok(Self(s.to_string()))
     }
