d24740bdd1bcebcbaf0bc5fe9a5d718b16cf4d8d
Spencer Wilson <5624115+spencerwilson@users.noreply.github.com>
2023-10-20 08:45:18 -0700

    track missing file

---
+++ src/command/package.rs
@@ -0,0 +1,62 @@
+use anyhow::Result;
+use clap::Subcommand;
+use comfy_table::Table;
+
+use crate::{api::Client, session};
+
+#[derive(Debug, Subcommand)]
+pub enum PackageAction {
+    /// List packages this account is authorized to use (build, query).
+    List,
+}
+
+pub async fn list() -> Result<()> {
+    let token = session::get_token()?;
+    let client = Client::new(&token)?;
+
+    let mut response = client.list_packages().await?;
+    response
+        .packages
+        .sort_unstable_by(|a, b| a.name.cmp(&b.name));
+    let rows: Vec<Vec<String>> = response
+        .packages
+        .into_iter()
+        .flat_map(|mut p| {
+            p.package_versions
+                .sort_unstable_by(|a, b| b.version.cmp(&a.version));
+            p.package_versions
+                .iter()
+                .map(|pv| -> Vec<String> {
+                    vec![
+                        p.name.to_owned(),
+                        pv.version.to_string(),
+                        if pv.accelerated {
+                            "Accelerated".into()
+                        } else {
+                            "Direct".into()
+                        },
+                        pv.patch_state
+                            .as_ref()
+                            .map(|s| {
+                                if let Some(data) = pv.patch_state_data.as_ref() {
+                                    format!("{}: {}", s, data)
+                                } else {
+                                    s.to_string()
+                                }
+                            })
+                            .unwrap_or("n/a".into()),
+                    ]
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let mut table = Table::new();
+    table.load_preset(comfy_table::presets::UTF8_FULL);
+    table.set_header(vec!["Name", "Version", "Performance mode", "State"]);
+    table.add_rows(rows);
+
+    println!("{table}");
+
+    Ok(())
+}
