f074f9d954f73a6c648c3f71d29a934ec501fa32
Spencer Wilson <5624115+spencerwilson@users.noreply.github.com>
2023-11-14 18:41:20 -0800

    drop tonic references in build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .compile(&["proto/dpm_agent.proto"], &["proto"])?;
-
-    tonic_build::compile_protos("proto/dpm_agent.proto")?;
-
     built::write_built_file().expect("Failed to acquire build-time information");
 
     Ok(())
